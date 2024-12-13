767febd051b35605b2228a4e12fee3f43bc327f1
mulyatma <mulyaatmaja604@gmail.com>
2024-12-13 14:18:04 +0700

    add auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->middleware('auth');
 
 Route::get('/mentoring', function () {
     return view('mentoring');

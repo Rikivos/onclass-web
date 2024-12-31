2f972df6464b9739692dfc9b11e278ddaacddbe9
mulyatma <mulyaatmaja604@gmail.com>
2024-12-31 15:24:12 +0700

    fix return controller logbook

--- app/Http/Controllers/LogbookController.php
+++ app/Http/Controllers/LogbookController.php
@@ -39,7 +39,7 @@ class LogbookController extends Controller
             'reports' => $reports
         ];
 
-        return view('mentee.logbook', compact('data'));
+        return view('mentor.logbook', compact('data'));
     }
 
 
